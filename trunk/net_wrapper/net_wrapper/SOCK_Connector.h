15616239d2e5e8e635a5eedd28e3a9dae21502c6
mariasolercliment <mariasolercliment@77f6bdef-6155-0410-8b08-fdea0229669f>
2008-09-01 19:53:21 +0000

    changes made in the client.cpp, to make it compile.

--- trunk/net_wrapper/net_wrapper/SOCK_Connector.h
+++ trunk/net_wrapper/net_wrapper/SOCK_Connector.h
@@ -8,5 +8,5 @@ class SOCK_Connector
 public:
 	SOCK_Connector(void);
 	~SOCK_Connector(void);
-	connect( SOCK_Stream stream, INET_Addr addr );
+	void connect( SOCK_Stream stream, INET_Addr addr );
 };
