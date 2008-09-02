696059ade0c8647807156d2113d6c15a3d74db51
kim.hedegaard.madsen <kim.hedegaard.madsen@77f6bdef-6155-0410-8b08-fdea0229669f>
2008-09-02 14:45:34 +0000

    adding code for reactor_server

---
+++ exercise_2/code/reactor_server/net_wrapper/SOCK_Connector.cpp
@@ -0,0 +1,15 @@
+#include "StdAfx.h"
+#include "SOCK_Connector.h"
+
+SOCK_Connector::SOCK_Connector(void)
+{
+}
+
+SOCK_Connector::~SOCK_Connector(void)
+{
+}
+
+void SOCK_Connector::connect( SOCK_Stream stream, INET_Addr addr )
+{
+
+}
