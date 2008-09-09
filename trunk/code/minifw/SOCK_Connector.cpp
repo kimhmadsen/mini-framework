3a521c4405d731e05939fba6c435b45a7dc70352
kim.hedegaard.madsen <kim.hedegaard.madsen@77f6bdef-6155-0410-8b08-fdea0229669f>
2008-09-09 14:02:38 +0000

    code the code from exercise 2 to trunk/code

---
+++ trunk/code/minifw/SOCK_Connector.cpp
@@ -0,0 +1,23 @@
+#include "StdAfx.h"
+#include "SOCK_Connector.h"
+
+SOCK_Connector::SOCK_Connector(void)
+{
+}
+
+SOCK_Connector::~SOCK_Connector(void)
+{
+}
+
+void SOCK_Connector::connect( SOCK_Stream &stream, INET_Addr addr )
+{
+	int status;
+	stream.get_handle();
+	stream.set_handle( ::socket(PF_INET, SOCK_STREAM, 0) ); // do some error checking!
+
+	status = ::connect( stream.get_handle(), addr.addr(), addr.size() );
+    if (status != 0) 
+	{
+        fprintf(stderr, "Connect failed.\n");
+    }
+}
