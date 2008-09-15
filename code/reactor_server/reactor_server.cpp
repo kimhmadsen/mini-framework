06f86ab3b29c8508ba85cb251790dfba167dce1b
kim.hedegaard.madsen <kim.hedegaard.madsen@77f6bdef-6155-0410-8b08-fdea0229669f>
2008-09-15 20:32:26 +0000

    renamed ConnectionAcceptor to LogAcceptor.

--- code/reactor_server/reactor_server.cpp
+++ code/reactor_server/reactor_server.cpp
@@ -3,10 +3,32 @@
 
 #include "stdafx.h"
 #include "INET_Addr.h"
+#include "Reactor.h"
+
+const short LOG_PORT    = 10000;
+const short ALARM_PORT  = 10001;
+const short PATVAL_PORT = 10002;
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	INET_Addr i_addr( 10000 );
+	INET_Addr log_addr( LOG_PORT );
+	INET_Addr alarm_addr( ALARM_PORT );
+	INET_Addr parient_addr( PATVAL_PORT );
+
+	// create connection acceptors
+
+
+	// add the acceptors to the reactor
+
+
+	// do the server loop
+	for(;;)
+	{
+		Reactor::instance()->handle_events( 100 );
+		// do timeout things 
+	}
+
 	return 0;
 }
 
