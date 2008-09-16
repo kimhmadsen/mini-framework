8916bb66a8f2c55db3df80099402caff30a63c12
kim.hedegaard.madsen <kim.hedegaard.madsen@77f6bdef-6155-0410-8b08-fdea0229669f>
2008-09-16 14:20:22 +0000

    It links!!!

--- trunk/code/minifw/Reactor_Implementation.cpp
+++ trunk/code/minifw/Reactor_Implementation.cpp
@@ -1,23 +1,6 @@
 #include "StdAfx.h"
 #include "Reactor_Implementation.h"
 
-void Reactor_Implementation::register_handler(Event_Handler *eh, Event_Type et)
-{
-}
-
-void Reactor_Implementation::register_handler(HANDLE h, Event_Handler *eh, Event_Type et)
-{
-}
-
-void Reactor_Implementation::remove_handler(Event_Handler *eh, Event_Type et)
-{
-}
-
-void Reactor_Implementation::remove_handler(HANDLE h, Event_Type et) const
-{
-}
-
-
 Reactor_Implementation::Reactor_Implementation(void)
 {
 }
