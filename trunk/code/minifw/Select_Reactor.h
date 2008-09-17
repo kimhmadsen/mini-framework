e1b75f573cdc1e00f92895708bdbef5c3a6826f7
kim.hedegaard.madsen <kim.hedegaard.madsen@77f6bdef-6155-0410-8b08-fdea0229669f>
2008-09-17 17:12:46 +0000

    adding files for select reactor.

---
+++ trunk/code/minifw/Select_Reactor.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "reactor.h"
+#include "select_reactor_implementation.h"
+
+class Select_Reactor :
+	public Reactor
+{
+public:
+	virtual void register_handler(Event_Handler *eh, Event_Type et);
+	virtual void register_handler(HANDLE h, Event_Handler *eh, Event_Type et);
+	virtual void remove_handler(Event_Handler *eh, Event_Type et);
+	virtual void remove_handler(HANDLE h, Event_Type et) const;
+
+	void handle_events(TIMEVAL *timeout =0)
+	{
+		// TODO: implementation; 
+	}
+
+	static Reactor* instance(void);
+protected:
+	Select_Reactor(void)
+	{
+		reactor_impl_ = new Select_Reactor_Implementation();
+	}
+	~Select_Reactor(void);
+
+private:
+
+	static Select_Reactor* instance_;
+	Reactor_Implementation* reactor_impl_;
+};
