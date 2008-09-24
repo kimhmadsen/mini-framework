bb0364b89e8b449077154ff62097596e209e89d8
kim.hedegaard.madsen <kim.hedegaard.madsen@77f6bdef-6155-0410-8b08-fdea0229669f>
2008-09-24 20:36:06 +0000

    Work done on reactor_server.

--- code/reactor_server/LogAcceptor.h
+++ code/reactor_server/LogAcceptor.h
@@ -8,35 +8,14 @@ class LogAcceptor :
 	public EventHandler
 {
 public:
-	LogAcceptor( InetAddr &addr, Reactor *reactor ):
-	  acceptor_ ( addr ), reactor_ ( reactor )
-	  {
-		  reactor_->RegisterHandler( this, ACCEPT_EVENT );
-	  }
+	LogAcceptor( InetAddr &addr, Reactor *reactor );
 	~LogAcceptor(void);
 
-	virtual void HandleEvent(HANDLE handle, Event_Type et )
-	{
-		// Can only be called for an ACCEPT event.
-		if( et == ACCEPT_EVENT )
-		{
-			SockStream client_connection( handle );
-
-			acceptor_.accept( client_connection );
-
-			// create event handlers for the connection.
-			LogEventHandler* leh = new LogEventHandler( client_connection, reactor_ );
-		}
-	}
-
-	virtual HANDLE GetHandle(void)
-	{
-		return acceptor_.GetHandle();
-	}
+	virtual void HandleEvent(HANDLE handle, Event_Type et );
+	virtual HANDLE GetHandle(void);
 private:
 	// Socket factory that accepts client connections
-	SockAcceptor acceptor_;
-
+	SockAcceptor acceptor;
 	// Cached <Reactor>.
-	Reactor *reactor_;
+	Reactor *reactor;
 };
--- code/reactor_server/LogEventHandler.h
+++ code/reactor_server/LogEventHandler.h
@@ -8,36 +8,15 @@ class LogEventHandler :
 	public EventHandler
 {
 public:
-	LogEventHandler(const SockStream &stream, Reactor *reactor): peer_stream_( stream )
-	{
-		reactor->RegisterHandler( this, READ_EVENT );
-	}
+	LogEventHandler(const SockStream &stream, Reactor *reactor);
 	~LogEventHandler(void);
 
-	virtual void HandleEvent ( HANDLE handle, Event_Type event_type )
-	{
-		switch( event_type )
-		{
-		case READ_EVENT:
-			char buf[256];
-			// code handle short "short-reads" omitted.
-			peer_stream_.recv( buf, sizeof(buf), 0 );
-
-			// Write logging record to standard output
-			std::cout << buf;
-			break;
-
-		case CLOSE_EVENT:
-			peer_stream_.close();
-			break;
-		}
-		
-	}
-	virtual HANDLE GetHandle() 
-	{
-		return peer_stream_.GetHandle();
-	}
+	virtual void HandleEvent ( HANDLE handle, Event_Type event_type );
+	virtual HANDLE GetHandle();
 private:
 	// Receives logging records from a connected client
-	SockStream peer_stream_;
+	SockStream peerStream;
+		// Cached <Reactor>.
+	Reactor *reactor;
+
 };
