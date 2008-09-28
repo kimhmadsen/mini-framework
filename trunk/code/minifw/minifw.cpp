5f030d46319612affef53f0384ce2e940cecd9b5
kim.hedegaard.madsen <kim.hedegaard.madsen@77f6bdef-6155-0410-8b08-fdea0229669f>
2008-09-28 20:58:17 +0000

    Added the MiniFwInit to initialize the winsocket lib.

--- trunk/code/minifw/minifw.cpp
+++ trunk/code/minifw/minifw.cpp
@@ -2,5 +2,17 @@
 //
 
 #include "stdafx.h"
+#include "minifw.h"
+#include "iostream"
 
+void MiniFwInit(void)
+{
+	WSADATA wsaData;   // if this doesn't work
+    //WSAData wsaData; // then try this instead
 
+    if (WSAStartup(MAKEWORD(1, 1), &wsaData) != 0) {
+        fprintf(stderr, "WSAStartup failed.\n");
+        exit(1);
+    }
+
+}
--- trunk/code/reactor_server/reactor_server.cpp
+++ trunk/code/reactor_server/reactor_server.cpp
@@ -1,13 +1,13 @@
 // reactor_server.cpp : Defines the entry point for the console application.
 //
-
 #include "stdafx.h"
+#include "minifw.h"
 #include "inetaddr.h"
 #include "reactor.h"
 #include "selectreactor.h"
 #include "logacceptor.h"
 
-#define TRACE(x) (x)
+#define TRACE(x) 
 
 const short LOG_PORT    = 10000;
 const short ALARM_PORT  = 10001;
@@ -17,9 +17,14 @@ const short PATVAL_PORT = 10002;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+
+
 	InetAddr log_addr( LOG_PORT );
 	InetAddr alarm_addr( ALARM_PORT );
 	InetAddr parient_addr( PATVAL_PORT );
+
+	// initialize the frame work 
+	MiniFwInit(); 
 
 	// create connection acceptors
 	LogAcceptor la( log_addr, SelectReactor::instance() );
@@ -32,7 +37,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	for(;;)
 	{
 		TIMEVAL tv;
-		tv.tv_sec  = 0;
+		tv.tv_sec  = 10;
 		tv.tv_usec = 100000;
 		SelectReactor::instance()->HandleEvents( &tv );
 		// do timeout things 
