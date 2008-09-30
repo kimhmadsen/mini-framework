35405a11ce4ebc79a44e45246bedc2413a34b2a1
kim.hedegaard.madsen <kim.hedegaard.madsen@77f6bdef-6155-0410-8b08-fdea0229669f>
2008-09-30 21:22:47 +0000

    comitting code to display alarms in priority order.

--- code/reactor_server/AlarmEventHandler.h
+++ code/reactor_server/AlarmEventHandler.h
@@ -1,7 +1,18 @@
 #pragma once
+#include <vector>
+#include <list>
+#include <iostream>
+#include <string>
 #include "eventhandler.h"
 #include "reactor.h"
 #include "sockacceptor.h"
+
+struct AlarmEvent
+{
+	int prio;
+	char text[256];
+	time_t timestamp;
+};
 
 class AlarmEventHandler :
 	public EventHandler
@@ -14,8 +25,11 @@ public:
 	virtual HANDLE GetHandle(void);
 
 private:
+
 	// Receives logging records from a connected client
 	SockStream peerStream;
 	// Cached <Reactor>.
 	Reactor *reactor;
+
+	std::list<AlarmEvent> alarmList;
 };
