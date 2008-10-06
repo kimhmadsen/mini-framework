e04ce14c1028cd3cc6c7c13ceb7be9b7056a1998
kim.hedegaard.madsen <kim.hedegaard.madsen@77f6bdef-6155-0410-8b08-fdea0229669f>
2008-10-06 19:55:46 +0000

    reactor_client and reactor_server is working.

--- code/reactor_server/PatientValueEventHandler.cpp
+++ code/reactor_server/PatientValueEventHandler.cpp
@@ -19,12 +19,18 @@ void PatientValueEventHandler::HandleEvent ( HANDLE handle, Event_Type event_typ
 	switch( event_type )
 	{
 	case READ_EVENT:
-		char buf[1024];
+		char buf[256];
+		int result;
 		// code handle short "short-reads" omitted.
-		peerStream.recv( buf, sizeof(buf), 0 );
+		result = peerStream.recv( buf, sizeof(buf), 0 );
 
-		// Write logging record to standard output
-		std::cout << "PatientValueEvent: " << buf << "\n";
+		if( result > 0 )
+		{
+			buf[result] = '\0';
+
+			// Write logging record to standard output
+			std::cout << "PatientValueEvent: " << buf << "\n";
+		}
 		break;
 
 	case CLOSE_EVENT:
