ab616702a241c2dd29f4ccd2917301ea475e2ae1
mariasolercliment <mariasolercliment@77f6bdef-6155-0410-8b08-fdea0229669f>
2008-10-13 17:11:34 +0000

    Added skeleton for the PatientInfoEventHandler (it took me days to make it to compile and link... I feel stupid!!!)

---
+++ trunk/code/PatientServer/PatientInfoEventHandler.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vector>
+#include <list>
+#include <iostream>
+#include <string>
+#include "eventhandler.h"
+#include "reactor.h"
+#include "sockacceptor.h"
+
+class PatientInfoEventHandler :
+	public EventHandler
+{
+public:
+	PatientInfoEventHandler( const SockStream &stream, Reactor *reactor);
+	~PatientInfoEventHandler(void);
+
+	virtual void HandleEvent ( HANDLE handle, Event_Type event_type );
+	virtual HANDLE GetHandle();
+private:
+	// Receives logging records from a connected client
+	SockStream peerStream;
+		// Cached <Reactor>.
+	Reactor *reactor;
+
+};
+
