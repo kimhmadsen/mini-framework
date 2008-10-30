b78406cbddd1a314088c1ff74f78693d01f194cd
mariasolercliment <mariasolercliment@77f6bdef-6155-0410-8b08-fdea0229669f>
2008-10-30 15:58:59 +0000

    forgot to commit some changes to the client

--- trunk/code/linux/PatientClient/PatientClient.cpp
+++ trunk/code/linux/PatientClient/PatientClient.cpp
@@ -23,7 +23,7 @@ int main(int argc, char* argv[])
 	char *host;
 	string run = "run";
 	bool running = true;
-
+	int numberOfRequests = 1;
 	std::cout << "Reactor client starting\n";
 
 	/* initialize random seed: */
@@ -42,6 +42,11 @@ int main(int argc, char* argv[])
 	{
 		run = (char *)argv[2];
 	}
+	if(argc > 3)
+	{
+		numberOfRequests = atoi(argv[3]);
+		cout << numberOfRequests << endl;
+	}
 
 	InetAddr patientAddr( PATIENT_PORT,host );
 
@@ -54,27 +59,41 @@ int main(int argc, char* argv[])
 	while(running)
 	{
 		char buf[256];
-		std::cout << "Type the cpr number of a patient to get the information: \n";
-		std::cin.getline(buf, sizeof buf );
-		int count = std::cin.gcount();
-
-
+		memset(buf, 0, sizeof buf );
+		int count;
+		if(run.compare("run") == 0)
+		{
+			std::cout << "Type the cpr number of a patient to get the information: \n";
+			std::cin.getline(buf, sizeof buf );
+			count = std::cin.gcount();
+		}
+		else //testing...
+		{
+			char cpr[] = "090382-3542";
+			strcpy(buf, cpr);
+			count = strlen(buf);
+		}
 		patientStream.send_n (buf, count, 0);
 		char buf_in[256];
-
+		memset(buf_in, 0, sizeof buf_in );
 		count = patientStream.recv(buf_in, sizeof buf_in, 0);
 		std::cout << "\n" << buf_in << std::endl;
 		i++;
 
-		if(run.compare("run")!= 0 && i == 50) //run == "test1" or run == "test2"
+		if(run.compare("run")!= 0 && i == numberOfRequests) //run == "test1" or run == "test2"
 			running = false;
 
 		if(run.compare("test2")==0 && running)
 		{
-			SockStream patientStream;
+			patientConnector.close(patientStream);
+			patientConnector.connect (patientStream, patientAddr);
+			std::cout << "Patient value stream connected on port " << PATIENT_PORT << " \n";
+
+/*			SockStream patientStream;
 			SOCK_Connector patientConnector;
 			patientConnector.connect (patientStream, patientAddr);
 			std::cout << "Patient value stream connected on port " << PATIENT_PORT << " \n";
+*/
 		}
 	}
 
