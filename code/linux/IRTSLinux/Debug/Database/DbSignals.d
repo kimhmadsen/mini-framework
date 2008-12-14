d018ebd43880f7af907e332ebe50490c202c05a6
kim.hedegaard.madsen <kim.hedegaard.madsen@77f6bdef-6155-0410-8b08-fdea0229669f>
2008-12-14 17:35:19 +0000

    removed some autogenerated files and added *.d to the ignore list.

--- code/linux/IRTSLinux/Debug/Database/DbSignals.d
+++
@@ -1,6 +0,0 @@
-Database/DbSignals.d Database/DbSignals.o: ../Database/DbSignals.cpp \
-  ../Database/DbSignals.h ../Database/../SignalValue.h
-
-../Database/DbSignals.h:
-
-../Database/../SignalValue.h:
